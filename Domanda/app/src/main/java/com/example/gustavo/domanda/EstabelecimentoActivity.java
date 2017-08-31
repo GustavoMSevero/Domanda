934f861e74ea0108228298f726f9dbf587971bfe
Gustavo Severo <Gustavo@MacBook-Pro.local>
2017-08-31 15:30:08 -0300

    Mudado a cor do fundo e dos botões. Modificado a tela de cadastro com apenas, nome, sobrenome, email e senha. Cadastro funcionando, mas sem validação dos campos

--- Domanda/app/src/main/java/com/example/gustavo/domanda/EstabelecimentoActivity.java
+++ Domanda/app/src/main/java/com/example/gustavo/domanda/EstabelecimentoActivity.java
@@ -28,15 +28,6 @@ public class EstabelecimentoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
         listEstabelecimentos = ((ListView)findViewById(R.id.lvEstabelecimentos));
 
     }
